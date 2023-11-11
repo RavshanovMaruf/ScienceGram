400811d2f69bce25f8b47324d28251c8776f45c8
Khasan Rashidov <khasanrashidov2002@gmail.com>
2023-11-11 10:40:21 +0500

    [1] - project setup (not full)

---
+++ ScienceGram.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ScienceGram.Infrastructure
+{
+	public class Class1
+	{
+
+	}
+}
---
+++ ScienceGram.Infrastructure/Identity/IdentityService.cs
@@ -0,0 +1,18 @@
+﻿using ScienceGram.Application.Common.Interfaces;
+using ScienceGram.Application.Common.Models.Account;
+
+namespace ScienceGram.Infrastructure.Identity
+{
+	public class IdentityService : IIdentityService
+	{
+		public Task<LoginResponse> LoginUserAsync(LoginViewModel userModel)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<RegisterResponse> RegisterUserAsync(RegisterViewModel model)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
